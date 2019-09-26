9eb9cc6ebfe9b9fcca926dfe824430219ed5641b
ting2313 <ting2313@gmail.com>
2019-09-26 12:50:00 +0800

    Q4

--- hw1/Q3.py
+++ hw1/Q3.py
@@ -6,6 +6,8 @@ x = []
 mean = []
 middle = []
 deviation = []
+low_mean = 0
+low_wap = -1
 
 with open('TrainingData_new.csv',newline='') as csvfile:
     rows = csv.DictReader(csvfile)
@@ -21,9 +23,13 @@ with open('TrainingData_new.csv',newline='') as csvfile:
 
     for num in range(0,520) :
         mean.append(np.mean(data[num]))
+        if low_mean>np.mean(data[num]):
+            low_mean = np.mean(data[num])
+            low_wap = num+1
         middle.append(np.median(data[num]))
         deviation.append(np.std(data[num]))
 
+print('low_wap:%d\n' % low_wap)
 fig,ax=plt.subplots(figsize=(20,10))
 plt.plot(x,mean,'r^-',x,middle,'g^-',x,deviation,'b^-')
 plt.legend(['mean','middle','standard deviation'])
--- hw1/Q4.py
+++ hw1/Q4.py
@@ -1,32 +1,30 @@
 import csv
 import matplotlib.pyplot as plt
-import numpy as np
+# lowest mean of WAP is WAP087
 
-x = []
-mean = []
-middle = []
-deviation = []
+x = ['0','1','2']
+b0 = 0
+b1 = 0
+b2 = 0
 
 with open('TrainingData_new.csv',newline='') as csvfile:
     rows = csv.DictReader(csvfile)
-    
-    data = []
-    for num in range(1,521):
-        data.append([])
-        x.append('WAP%03d' % num)
     for row in rows:
-        for num in range(1,521) :
-            key = 'WAP%03d' % num
-            data[num-1].append(int(row[key]))
+        if row['WAP087']!='0':
+            build = row['BUILDINGID']
+            if build=='0':
+                b0+=1
+            elif build=='1':
+                b1+=1
+            elif build=='2':
+                b2+=1
 
-    for num in range(0,520) :
-        mean.append(np.mean(data[num]))
-        middle.append(np.median(data[num]))
-        deviation.append(np.std(data[num]))
-
-fig,ax=plt.subplots(figsize=(20,10))
-plt.plot(x,mean,'r^-',x,middle,'g^-',x,deviation,'b^-')
-plt.title("Q3")
-plt.xlabel("No. of WAP")
-fig.autofmt_xdate()
+sum = b0+b1+b2
+pdf = [b0/sum,b1/sum,b2/sum]
+cdf = [b0/sum,(b0+b1)/sum,1]
+plt.plot(x,pdf,'b^',x,cdf,'g-')
+plt.legend(['pdf','cdf'])
+plt.title("Q4")
+plt.xlabel("ID of building")
+plt.ylabel("Number of RSSI Value")
 plt.show()
