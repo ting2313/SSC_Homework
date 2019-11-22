fd97aa72d85b8046c5f0aef6e5bb3d9de01ad148
ting2313 <ting2313@gmail.com>
2019-11-22 21:05:49 +0800

    usage

--- hw3/main.py
+++ hw3/main.py
@@ -9,24 +9,30 @@ import numpy as np
 import time
 import threading
 AVERAGE_ARRIVE_TIME = 4
-SERVING_NUM = 5
+SERVING_NUM = 10
 
+# Creating an object of server and start at another threading
 server = server.Server()
-clientCreateTime = np.random.exponential(scale=AVERAGE_ARRIVE_TIME,size=SERVING_NUM)
 serverThread = threading.Thread(target=server.start,args=[SERVING_NUM])
 serverThread.start()
 print("Create Thread Successfully")
+
+# Creating customers which following exponential distribution
 startTime = time.time()
-print(clientCreateTime)
+clientCreateTime = np.random.exponential(scale=AVERAGE_ARRIVE_TIME,size=SERVING_NUM)
 for clientTime in clientCreateTime :
     while (time.time()-startTime)<clientTime:
         continue
     newCustomer = customer.Customer()
     server.queue.push(newCustomer)
-    print("Add Customer")
 
+# Waiting for the server
 serverThread.join()
+totalServerTime = time.time()-startTime
 print("End Simulation")
 
+# Evaluating
 print("Average Waiting Time:")
 print(server.queue.totalWaitingTime/SERVING_NUM)
+print("Usage:")
+print(server.servingTime/totalServerTime)
