53e9397b18f51c6037af53b5110daefd877f9670
ting2313 <ting2313@gmail.com>
2019-11-22 19:55:55 +0800

    ver1

--- hw3/customer.py
+++ hw3/customer.py
@@ -1,8 +1,8 @@
 import numpy as np
 import time
-AVERAGE_TIME = 3
+AVERAGE_SERV_TIME = 3
 
 class Customer:
     def __init__(self):
-        self.processTime = np.random.exponential(scale=AVERAGE_TIME)
+        self.processTime = np.random.exponential(scale=AVERAGE_SERV_TIME)
         self.createTime = time.time()
--- hw3/main.py
+++ hw3/main.py
@@ -1,2 +1,9 @@
 import customer
 import server
+import numpy as np
+AVERAGE_ARRIVE_TIME = 4
+SERVING_NUM = 10
+
+server = server.Server()
+clientCreateTime = np.random.exponential(scale=AVERAGE_ARRIVE_TIME,size=SERVING_NUM)
+server.start(SERVING_NUM)
